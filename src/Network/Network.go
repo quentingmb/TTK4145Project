80d7239ca4d7129294c451ff7f70cbdd6b5a7a9b
hichamel <hicham.elharem@telecom-bretagne.eu>
2016-04-14 02:32:25 +0200

    Update Network.go

--- src/Network/Network.go
+++ src/Network/Network.go
@@ -1,57 +1,24 @@
 package Network
 
 import (
-	"driver"
 	"Elevator"
-	"encoding/json"
+	"driver"
+	//"encoding/json"
+	"extra"
 	"fmt"
 	"log"
-	"extra"
 	"net"
 	"os"
 	"strings"
 	"time"
 	//"io/ioutil"
-	"os/exec"
-	)
+	//"os/exec"
+)
 
- type Request struct {
- 	Direction Elevator.Elev_button
- 	Floor int
- 	Type int //internal or external
- 	Ipsource string 
- }
- type Info struct {
- 	PreviousFloor int
- 	Inhouse bool
- 	State string 
- 	Ipsource string
- }
- type ElevatorMessage struct {
- 	Request Request
- 	Info   Info
- }
-
- type Con struct {
- 	Address *net.TCPConn
-	Connected bool
- }
-var NoRequest=[]Request{Request{}}
-var requestlist=make([]Request,0)
+var requestlist = make([]Request, 0)
 var elevators = make(map[string]bool)
 var infolist = make(map[string]Info)
-var connections = make([]*net.TCPConn, 0)
-
-func GetLocalIP() string {
-	oneliner := "ifconfig | grep 129.241.187 | cut -d':' -f2 | cut -d' ' -f1" //Favourite Oneliner
-	cmd := exec.Command("bash", "-c", oneliner)
-	out, err := cmd.Output()
-	if err != nil {
-		fmt.Println(err)
-	}
-	ip := strings.TrimSpace(string(out))
-	return ip
-}
+var tcp_connections = make([]*net.TCPConn, 0)
 
 func GetInfoList() map[string]Info {
 	return infolist
@@ -61,45 +28,28 @@ func GetRequestList() []Request {
 	return requestlist
 }
 
-func PackElevatorMessage(message ElevatorMessage, error_c chan string) []byte {
-	send, err := json.Marshal(message)
-	if err != nil {
-		error_c <- "Could not pack message: " + err.Error()
-	}
-	return send
-}
-
-func UnpackElevatorMessage(packed []byte, error_c chan string) ElevatorMessage {
-	var message ElevatorMessage
-	err := json.Unmarshal(packed, &message)
-	if err != nil {
-		error_c <- "Error in unpacking the message: " + err.Error()
-	}
-	return message
-}
-
-func InitUpdate(connection *net.TCPConn, localip string, error_c chan string) {
+func InitUpdate(connection *net.TCPConn, localip string, Error chan string) {
 	pack := make([]byte, 1024)
 	info := infolist[localip]
-	pack = PackElevatorMessage(ElevatorMessage{Request: Request{}, 
-	Info: info}, error_c)
+	pack = PackElevatorMessage(ElevatorMessage{Request: Request{},
+		Info: info}, Error)
 	time.Sleep(10 * time.Millisecond)
 	connection.Write(pack)
 	for _, request := range requestlist {
 		time.Sleep(10 * time.Millisecond)
-		pack = PackElevatorMessage(ElevatorMessage{Request: request, 
-		Info: Info{}}, error_c)
+		pack = PackElevatorMessage(ElevatorMessage{Request: request,
+			Info: Info{}}, Error)
 		connection.Write(pack)
 	}
 }
 
 //Check if there is any new orders, if it is it passes it to Neworder
-func Requestdistr(generatedMsgs_c chan ElevatorMessage, localip string) {
+func RequestChecker(generatedMsgs chan ElevatorMessage, localip string) {
 	var button Elevator.Elev_button
 	for {
 		for floor, buttons := range Elevator.Button_channel_matrix {
 			for butt, channel := range buttons {
-				if driver.Read_bit(channel)!=0 {
+				if driver.Read_bit(channel) != 0 {
 					if butt == 0 {
 						button = Elevator.BUTTON_CALL_UP
 					} else if butt == 1 {
@@ -107,7 +57,7 @@ func Requestdistr(generatedMsgs_c chan ElevatorMessage, localip string) {
 					} else {
 						button = Elevator.BUTTON_COMMAND
 					}
-					NewRequest(generatedMsgs_c, Request{Direction: button, Floor: floor + 1, Type: 1, Ipsource: localip})
+					NewRequest(generatedMsgs, Request{Direction: button, Floor: floor + 1, Type: 1, Ipsource: localip})
 					time.Sleep(time.Millisecond)
 				}
 			}
@@ -115,115 +65,44 @@ func Requestdistr(generatedMsgs_c chan ElevatorMessage, localip string) {
 	}
 }
 
-func Dialer(connect_c chan Con, port string, elevators []extra.Elevator, error_c chan string) {
-	local, _ := net.ResolveTCPAddr("tcp", "localhost"+port)
-	localconn, _ := net.DialTCP("tcp", nil, local)
-	connect_c <- Con{Address: localconn, Connected: true}
-	for {
-	elevatorloop:
-		for _, elevator := range elevators {
-			cons := connections
-			for _, connection := range cons {
-				if strings.Split(connection.RemoteAddr().String(), ":")[0] == elevator.Address {
-					continue elevatorloop
-				}
-			}
-			raddr, err := net.ResolveTCPAddr("tcp", elevator.Address+port)
-			dialConn, err := net.DialTCP("tcp", nil, raddr)
-			if err != nil {
-				error_c <- "Dial trouble: " + err.Error()
-			} else {
-				connect_c <- Con{Address: dialConn, Connected: true}
-			}
-		}
-		time.Sleep(1000 * time.Millisecond)
-	}
-}
-
-func Listener(conn *net.TCPListener, connect_c chan Con, error_c chan string) {
-	for {
-		newConn, err := conn.AcceptTCP()
-		if err != nil {
-			error_c <- "Accept trouble: " + err.Error()
-		}
-		connect_c <- Con{Address: newConn, Connected: true}
-	}
-}
-
-func Receiver(conn *net.TCPConn, receivedMsgs_c chan ElevatorMessage, connections_c chan Con, error_c chan string) {
-	buf := make([]byte, 1024)
-	keepalivebyte := []byte("KEEPALIVE")
-receiverloop:
-	for {
-		err := conn.SetReadDeadline(time.Now().Add(2 * time.Second))
-		if err != nil {
-			error_c <- "Trouble setting read deadline: " + err.Error()
-			connections_c <- Con{Address: conn, Connected: false}
-			return
-		}
-		bit, err := conn.Read(buf[0:])
-		if err != nil {
-			error_c <- "Trouble receiving: " + err.Error()
-			connections_c <- Con{Address: conn, Connected: false}
-			return
-		}
-		if string(buf[:bit]) == string(keepalivebyte) {
-			continue receiverloop
-		}
-		unpacked := UnpackElevatorMessage(buf[:bit], error_c)
-		receivedMsgs_c <- unpacked
-	}
-}
-
-func SendAliveMessages(connection *net.TCPConn, error_c chan string) {
-	for {
-		_, err := connection.Write([]byte("KEEPALIVE"))
-		if err != nil {
-			error_c <- "error in sending keepalive message: " + err.Error()
-			return
-		}
-		time.Sleep(time.Second)
-	}
-}
-
-func TCPPeerToPeer(conf extra.Config, localip string, generatedmessages_c chan ElevatorMessage) {
+func NetworkManager(conf extra.Config, localip string, generatedMsgs chan ElevatorMessage) {
 	elevlog, err := os.OpenFile("elevator.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		fmt.Println("Error opening file: " + err.Error())
 	}
 	defer elevlog.Close()
 	log.SetOutput(elevlog)
-	listenaddr, _ := net.ResolveTCPAddr("tcp", conf.DefaultListenPort)
-	listenconn, _ := net.ListenTCP("tcp", listenaddr)
-	connections_c := make(chan Con, 15)
-	receivedmessages_c := make(chan ElevatorMessage, 15)
-	error_c := make(chan string, 10)
-	go Listener(listenconn, connections_c, error_c)
-	go Requestdistr(generatedmessages_c, localip)
-	go Dialer(connections_c, conf.DefaultListenPort, conf.Elevators, error_c)
+	listenAdress, _ := net.ResolveTCPAddr("tcp", conf.DefaultListenPort)
+	listenConnection, _ := net.ListenTCP("tcp", listenAdress)
+	connections := make(chan Connection, 15)
+	receivedMsgs := make(chan ElevatorMessage, 15)
+	Error := make(chan string, 10)
+	go Listener(listenConnection, connections, Error)
+	go RequestChecker(generatedMsgs, localip)
+	go New_Connection(connections, conf.DefaultListenPort, conf.Elevators, Error)
 	for {
 		select {
-		case connection := <-connections_c: //Managing new/closed connections
+		case connection := <-connections: //Managing new/closed connections
 			{
 				if connection.Connected {
-					connections = append(connections, connection.Address)
-					go Receiver(connection.Address, receivedmessages_c, connections_c, error_c)
-					go SendAliveMessages(connection.Address, error_c)
-					go InitUpdate(connection.Address, localip, error_c)
+					tcp_connections = append(tcp_connections, connection.Addr)
+					go Receiver(connection.Addr, receivedMsgs, connections, Error)
+					go AliveNotification(connection.Addr, Error)
+					go InitUpdate(connection.Addr, localip, Error)
 				} else {
-					remoteip := strings.Split(connection.Address.RemoteAddr().String(), ":")[0]
+					remoteip := strings.Split(connection.Addr.RemoteAddr().String(), ":")[0]
 					errorstate := Info{State: "ERROR", PreviousFloor: 0, Inhouse: false, Ipsource: remoteip}
 					infolist[remoteip] = errorstate
-					for i, con := range connections {
-						if con == connection.Address {
-							connections[len(connections)-1], connections[i], connections = nil, connections[len(connections)-1], connections[:len(connections)-1]
+					for i, conn := range tcp_connections {
+						if conn == connection.Addr {
+							tcp_connections[len(tcp_connections)-1], tcp_connections[i], tcp_connections = nil, tcp_connections[len(tcp_connections)-1], tcp_connections[:len(connections)-1]
 						}
 					}
-					connection.Address.Close()
+					connection.Addr.Close()
 				}
 
 			}
-		case received := <-receivedmessages_c:
+		case received := <-receivedMsgs:
 			{
 				if received.Request.Floor > 0 {
 					if !((received.Request.Direction == Elevator.BUTTON_COMMAND) && (received.Request.Ipsource != localip)) {
@@ -255,18 +134,18 @@ func TCPPeerToPeer(conf extra.Config, localip string, generatedmessages_c chan E
 					infolist[received.Info.Ipsource] = received.Info
 				}
 			}
-		case message := <-generatedmessages_c:
+		case message := <-generatedMsgs:
 			{
 				pack := make([]byte, 1024)
-				pack = PackElevatorMessage(message, error_c)
-				for _, connection := range connections {
+				pack = PackElevatorMessage(message, Error)
+				for _, connection := range tcp_connections {
 					_, err := connection.Write(pack)
 					if err != nil {
-						error_c <- "Problems writing to connection: " + err.Error()
+						Error <- "Problems writing to connection: " + err.Error()
 					}
 				}
 			}
-		case err := <-error_c:
+		case err := <-Error:
 			{
 				log.Println("ERROR: " + err)
 			}
@@ -274,23 +153,23 @@ func TCPPeerToPeer(conf extra.Config, localip string, generatedmessages_c chan E
 	}
 }
 
-func SendStatuslist(generatedMsgs_c chan ElevatorMessage) {
+func SendStatuslist(generatedMsgs chan ElevatorMessage) {
 	localip := GetLocalIP()
 	myinfo := infolist[localip]
-	generatedMsgs_c <- ElevatorMessage{Request: Request{}, Info: myinfo}
+	generatedMsgs <- ElevatorMessage{Request: Request{}, Info: myinfo}
 }
 
-func NewInfo(info Info, generatedMsgs_c chan ElevatorMessage) bool {
+func NewInfo(info Info, generatedMsgs chan ElevatorMessage) bool {
 	for _, oldinfo := range infolist {
 		if oldinfo == info {
 			return false
 		}
 	}
-	generatedMsgs_c <- ElevatorMessage{Request: Request{}, Info: info}
+	generatedMsgs <- ElevatorMessage{Request: Request{}, Info: info}
 	return true
 }
 
-func NewRequest(generatedMsgs_c chan ElevatorMessage, request Request) bool {
+func NewRequest(generatedMsgs chan ElevatorMessage, request Request) bool {
 	if request.Direction != Elevator.BUTTON_COMMAND {
 		request.Ipsource = ""
 	}
@@ -299,6 +178,6 @@ func NewRequest(generatedMsgs_c chan ElevatorMessage, request Request) bool {
 			return false
 		}
 	}
-	generatedMsgs_c <- ElevatorMessage{Request: request, Info: Info{}}
+	generatedMsgs <- ElevatorMessage{Request: request, Info: Info{}}
 	return true
 }
