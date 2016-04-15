320958e5e1299c3428626e159c485225c3a2161a
quentingmb <quentin.gombia@telecom-bretagne.eu>
2016-04-15 16:53:24 +0200

    Update Network.go

--- src/Network/Network.go
+++ src/Network/Network.go
@@ -43,8 +43,8 @@ func InitUpdate(connection *net.TCPConn, localip string, Error chan string) {
 	}
 }
 
-//Check if there is any new orders, if it is it passes it to Neworder
-func RequestChecker(generatedMsgs chan ElevatorMessage, localip string) {
+//Check if there is any new requests, if it is, it passes it to NewRequest
+func RequestUpdate(ToSendMsgs chan ElevatorMessage, localip string) {
 	var button Elevator.Elev_button
 	for {
 		for floor, buttons := range Elevator.Button_channel_matrix {
@@ -57,7 +57,7 @@ func RequestChecker(generatedMsgs chan ElevatorMessage, localip string) {
 					} else {
 						button = Elevator.BUTTON_COMMAND
 					}
-					NewRequest(generatedMsgs, Request{Direction: button, Floor: floor + 1, Type: 1, Ipsource: localip})
+					NewRequest(ToSendMsgs, Request{Direction: button, Floor: floor + 1, Type: 1, Ipsource: localip})
 					time.Sleep(time.Millisecond)
 				}
 			}
@@ -65,7 +65,7 @@ func RequestChecker(generatedMsgs chan ElevatorMessage, localip string) {
 	}
 }
 
-func NetworkManager(conf extra.Config, localip string, generatedMsgs chan ElevatorMessage) {
+func NetworkManager(conf extra.Config, localip string, ToSendMsgs chan ElevatorMessage) {
 	elevlog, err := os.OpenFile("elevator.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		fmt.Println("Error opening file: " + err.Error())
@@ -78,7 +78,7 @@ func NetworkManager(conf extra.Config, localip string, generatedMsgs chan Elevat
 	receivedMsgs := make(chan ElevatorMessage, 15)
 	Error := make(chan string, 10)
 	go Listener(listenConnection, connections, Error)
-	go RequestChecker(generatedMsgs, localip)
+	go RequestUpdate(ToSendMsgs, localip)
 	go New_Connection(connections, conf.DefaultListenPort, conf.Elevators, Error)
 	for {
 		select {
@@ -91,7 +91,7 @@ func NetworkManager(conf extra.Config, localip string, generatedMsgs chan Elevat
 					go InitUpdate(connection.Addr, localip, Error)
 				} else {
 					remoteip := strings.Split(connection.Addr.RemoteAddr().String(), ":")[0]
-					errorstate := Info{State: "ERROR", PreviousFloor: 0, Inhouse: false, Ipsource: remoteip}
+					errorstate := Info{State: "ERROR", PreviousFloor: 0, Ipsource: remoteip}
 					infolist[remoteip] = errorstate
 					for i, conn := range tcp_connections {
 						if conn == connection.Addr {
@@ -102,42 +102,42 @@ func NetworkManager(conf extra.Config, localip string, generatedMsgs chan Elevat
 				}
 
 			}
-		case received := <-receivedMsgs:
+		case ReceivedMsg := <-receivedMsgs:
 			{
-				if received.Request.Floor > 0 {
-					if !((received.Request.Direction == Elevator.BUTTON_COMMAND) && (received.Request.Ipsource != localip)) {
-						Elevator.SetElevButtonLamp(received.Request.Direction, received.Request.Floor, received.Request.Type)
+				if ReceivedMsg.Request.Floor > 0 {
+					if !((ReceivedMsg.Request.Direction == Elevator.BUTTON_COMMAND) && (ReceivedMsg.Request.Ipsource != localip)) {
+						Elevator.SetElevButtonLamp(ReceivedMsg.Request.Direction, ReceivedMsg.Request.Floor, ReceivedMsg.Request.Type)
 					}
-					if received.Request.Direction != Elevator.BUTTON_COMMAND {
-						received.Request.Ipsource = ""
+					if ReceivedMsg.Request.Direction != Elevator.BUTTON_COMMAND {
+						ReceivedMsg.Request.Ipsource = ""
 					}
-					if received.Request.Type == 0 {
-						received.Request.Type = 1
+					if ReceivedMsg.Request.Type == 0 {
+						ReceivedMsg.Request.Type = 1
 						for i, b := range requestlist {
-							if b == received.Request {
+							if b == ReceivedMsg.Request {
 								requestlist = append(requestlist[:i], requestlist[i+1:]...)
 							}
 						}
 					} else {
-						AddedBefore := false
+						AlreadyExists := false
 						for _, b := range requestlist {
-							if b == received.Request {
-								AddedBefore = true
+							if b == ReceivedMsg.Request {
+								AlreadyExists = true
 							}
 						}
-						if !AddedBefore {
-							requestlist = append(requestlist, received.Request)
+						if !AlreadyExists {
+							requestlist = append(requestlist, ReceivedMsg.Request)
 						}
 					}
 				}
-				if received.Info.Ipsource != "" {
-					infolist[received.Info.Ipsource] = received.Info
+				if ReceivedMsg.Info.Ipsource != "" {
+					infolist[ReceivedMsg.Info.Ipsource] = ReceivedMsg.Info
 				}
 			}
-		case message := <-generatedMsgs:
+		case messageToSend := <-ToSendMsgs:
 			{
 				pack := make([]byte, 1024)
-				pack = PackElevatorMessage(message, Error)
+				pack = PackElevatorMessage(messageToSend, Error)
 				for _, connection := range tcp_connections {
 					_, err := connection.Write(pack)
 					if err != nil {
@@ -153,23 +153,18 @@ func NetworkManager(conf extra.Config, localip string, generatedMsgs chan Elevat
 	}
 }
 
-func SendStatuslist(generatedMsgs chan ElevatorMessage) {
-	localip := GetLocalIP()
-	myinfo := infolist[localip]
-	generatedMsgs <- ElevatorMessage{Request: Request{}, Info: myinfo}
-}
 
-func NewInfo(info Info, generatedMsgs chan ElevatorMessage) bool {
+func NewInfo(info Info, ToSendMsgs chan ElevatorMessage) bool {
 	for _, oldinfo := range infolist {
 		if oldinfo == info {
 			return false
 		}
 	}
-	generatedMsgs <- ElevatorMessage{Request: Request{}, Info: info}
+	ToSendMsgs <- ElevatorMessage{Request: Request{}, Info: info}
 	return true
 }
 
-func NewRequest(generatedMsgs chan ElevatorMessage, request Request) bool {
+func NewRequest(ToSendMsgs chan ElevatorMessage, request Request) bool {
 	if request.Direction != Elevator.BUTTON_COMMAND {
 		request.Ipsource = ""
 	}
@@ -178,6 +173,6 @@ func NewRequest(generatedMsgs chan ElevatorMessage, request Request) bool {
 			return false
 		}
 	}
-	generatedMsgs <- ElevatorMessage{Request: request, Info: Info{}}
+	ToSendMsgs <- ElevatorMessage{Request: request, Info: Info{}}
 	return true
 }
